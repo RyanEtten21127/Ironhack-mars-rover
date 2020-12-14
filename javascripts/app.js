55c73debdc3f8b51decf93f64c4d2edbb83d37b2
Ryan Etten <21127@columbushs.com>
2020-12-14 13:08:58 -0500

    mars rover

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,15 +1,49 @@
 // Rover Object Goes Here
 // ======================
+let rover = {
+  direction: "N",
+  x: 0,
+  y: 0,
+}
+
 
 // ======================
-function turnLeft(rover){
+function turnLeft(rover) {
   console.log("turnLeft was called!");
+  if (rover.direction == "N") {
+    rover.direction = "W";
+  }
+  else if (rover.direction == "W") {
+    rover.direction = "S";
+  }
+  else if (rover.direction == "S") {
+    rover.direction = "E";
+  }
+  else if (rover.direction == "E") {
+    rover.direction = "N";
+  }
 }
 
-function turnRight(rover){
+function turnRight(rover) {
   console.log("turnRight was called!");
+  if (rover.direction == "N") {
+    rover.direction = "E";
+  }
+  else if (rover.direction == "E") {
+    rover.direction = "S";
+  }
+  else if (rover.direction == "S") {
+    rover.direction = "W";
+  }
+  else if (rover.direction == "W") {
+    rover.direction = "N";
+  }
 }
 
-function moveForward(rover){
+// Iteration 3
+let roverPosition = ["x", "y"]
+
+function moveForward(rover) {
   console.log("moveForward was called")
 }
+
